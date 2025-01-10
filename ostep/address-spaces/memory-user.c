414740c3973fe6bdaa074a498539407b2960cdfa
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-01-09 23:46:43 -0600

    memory-user.c program

---
+++ ostep/address-spaces/memory-user.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+  long size = strtol(argv[1], NULL, 10);
+
+  int n = (size * 1000000) / sizeof(int);
+
+  int* arr;
+  arr = (int*) malloc(size);
+
+  if (arr == NULL) {
+    printf("Memory allocation failed!\n");
+    return 1;
+  }
+
+  for (int i = 0; i < n; i++) {
+    printf("arr %d: %p\n", i, &arr[i]);
+  }
+
+  return 0
+}
